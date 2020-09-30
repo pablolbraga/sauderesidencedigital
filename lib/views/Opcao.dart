54478856c262d2853db15fc723fd9ab36cdf15ae
Pablo Braga <pablolbraga@gmail.com>
2020-09-30 09:36:20 -0300

    Criação dos botões de acesso as telas de agenda e relatórios

--- lib/views/Opcao.dart
+++ lib/views/Opcao.dart
@@ -6,8 +6,65 @@ class Opcao extends StatefulWidget {
 }
 
 class _OpcaoState extends State<Opcao> {
+  void _abrirAgenda(){
+
+  }
+
+  void _abrirRelatorios(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Opções")
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xffD9EFF3)),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: RaisedButton(
+                    child: Text(
+                      "Agenda",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    color: Colors.blue,
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                    onPressed: () {
+                      _abrirAgenda();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: RaisedButton(
+                    child: Text(
+                      "Relatorios",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    color: Colors.blue,
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                    onPressed: () {
+                      _abrirRelatorios();
+                    },
+                  ),
+                ),
+              ]
+            )
+          )
+        )
+      ),
+    );
   }
 }
